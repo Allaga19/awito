de152fe8be8cea58dd7e2eda52a14c4ec0ee5ad8
Алла Гаврюшина <Allaga59yandex.ru>
2023-02-11 15:11:24 +0300

    В модальное окно добавлено событие. Закрытие окна по клику на клавишу Esc

--- js/script.js
+++ js/script.js
@@ -2,39 +2,58 @@
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
-const modalSubmit = document.querySelector('.modal-submit');
+const modalSubmit = document.querySelector('.modal__submit');
 const catalog = document.querySelector('.catalog');
 const modalItem = document.querySelector('.modal__item');
 
+
 // открытие и закрытие модального окна
-const closeModal = function (event) {
+const closeModal = function(event) {
 	const target = event.target;
-	if (target.closest('.modal__close') || target === this) {
+	if(target.closest('.modal__close') || target === this) {
 		this.classList.add('hide');
 		// зачистка формы
-		modalSubmit.reset();
+		if(this === modalAdd) {
+			console.log(modalAdd);
+			modalSubmit.reset();
+		}
+	}
+};
+// console.log(modalAdd);
+
+// Закрытие модального окна по кнопке Esc
+const closeModalEsc = event => {
+	// console.log('close');
+
+	if (event.code === 'Escape') {
+		modalAdd.classList.add('hide');
+		modalItem.classList.add('hide');
+		document.removeEventListener('keydown', closeModalEsc);
 	}
 };
 
-// console.log(modalAdd);
 // Отлавливаем событие click, модальное окно
 addAd.addEventListener('click', () => {
 	// открытие модального окна формы
 	modalAdd.classList.remove('hide');
 	//блокировка кнопки "отправить" в модальном окне
 	modalBtnSubmit.disabled = true;
+	// Закрытие модального окна по кнопке Esc
+	document.addEventListener('keydown', closeModalEsc);
 });
 
 // Закрытие модальных окон
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
 
-// получаем карточку 
+// получаем модальное окно карточки 
 catalog.addEventListener('click', (event) => {
 	const target = event.target;
 	if (target.closest('.card')) {
 		// открытие модального окна карточки
 		modalItem.classList.remove('hide');
+	// Закрытие модального окна по кнопке Esc
+	document.addEventListener('keydown', closeModalEsc);
 	}
-
 });
+
