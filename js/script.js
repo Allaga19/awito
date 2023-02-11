37ab6da6574ef47c3195e33cb8d28f2f5ca07168
Алла Гаврюшина <Allaga59yandex.ru>
2023-02-11 13:49:50 +0300

    Изменён код в closeModal. Использован this

--- js/script.js
+++ js/script.js
@@ -6,14 +6,11 @@ const modalSubmit = document.querySelector('.modal-submit');
 const catalog = document.querySelector('.catalog');
 const modalItem = document.querySelector('.modal__item');
 
-const closeModal = (event) => {
+// открытие и закрытие модального окна
+const closeModal = function (event) {
 	const target = event.target;
-	// открытие и закрытие модального окна
-	if (target.classList.contains('modal__close') ||
-	target === modalAdd || 
-	target === modalItem) {
-		modalAdd.classList.add('hide');
-		modalItem.classList.add('hide');
+	if (target.closest('.modal__close') || target === this) {
+		this.classList.add('hide');
 		// зачистка формы
 		modalSubmit.reset();
 	}
