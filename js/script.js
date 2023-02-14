9fb68e4e45f4465fd8e16b33f93465aa9378dd9c
Алла Гаврюшина <Allaga59yandex.ru>
2023-02-14 22:29:16 +0300

    localStorage

--- js/script.js
+++ js/script.js
@@ -1,7 +1,6 @@
 'use strict';
 // массив для объявления
-const dataBase = [];
-
+const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
 
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
@@ -15,23 +14,17 @@ const modalBtnWarning = document.querySelector('.modal__btn-warning');
 // получаем все элементы из формы
 const elementsModalSubmit = [...modalSubmit.elements]
 	.filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit');   // 58:40 // исключение кнопки
-// console.log(elementsModalSubmit);
+
+	// база  в localStoreage
+const saveDB = () => localStorage.setItem('awito', JSON.stringify(dataBase));
+console.log(dataBase);
 
 const checkForm = () => {
 	// прверка на заполнение всех input формы
 	const validForm = elementsModalSubmit.every(elem => elem.value);
-	// console.log(validForm);
 	// разблокировка кнопки "отправить" в модальном окне
 	modalBtnSubmit.disabled = !validForm;
 	modalBtnWarning.style.display = validForm ? 'none' : '';
-	/*
-// или подругому
-if (validForm) {
-	modalBtnWarning.style.display = 'none';
-} else {
-	modalBtnWarning.style.display = '';
-}
-*/
 };
 
 // закрытие модального окна
@@ -47,31 +40,10 @@ const closeModal = (event) => {
 		modalSubmit.reset();
 		checkForm();
 	}
-	// if(target.closest('.modal__close') || target === this) {
-	// 	this.classList.add('hide');
-	// 	// зачистка формы
-	// 	if(this === modalAdd) {
-	// 		console.log(modalAdd);
-	// 		modalSubmit.reset();
-	// 	}
-	// }
 };
-// console.log(modalAdd);
-/*
-// Закрытие модального окна по кнопке Esc
-const closeModalEsc = event => {
-	// console.log('close');
-	if (event.code === 'Escape') {
-		modalAdd.classList.add('hide');
-		modalItem.classList.add('hide');
-		document.removeEventListener('keydown', closeModalEsc);
-	}
-};
-*/
 // События для формы в модальном окне
 modalSubmit.addEventListener('input', checkForm);
 // избавляемся от перезагрузки страницы
-// блокируем стандартное поведение браузера
 modalSubmit.addEventListener('submit', event => {
 	event.preventDefault();
 	const itemObj = {};
@@ -80,16 +52,13 @@ modalSubmit.addEventListener('submit', event => {
 	}
 	dataBase.push(itemObj);
 	closeModal({ target: modalAdd });
-	// modalSubmit.reset();
+	saveDB();
 });
 
 // Отлавливаем событие click и показывает модальное окно
 addAd.addEventListener('click', () => {
-	// открытие модального окна формы
 	modalAdd.classList.remove('hide');
-	//блокировка кнопки "отправить" в модальном окне
 	modalBtnSubmit.disabled = true;
-	// Закрытие модального окна по кнопке Esc
 	document.addEventListener('keydown', closeModal);
 });
 
@@ -98,9 +67,7 @@ catalog.addEventListener('click', (event) => {
 	const target = event.target;
 
 	if (target.closest('.card')) {
-		// открытие модального окна карточки
 		modalItem.classList.remove('hide');
-		// Закрытие модального окна по кнопке Esc
 		document.addEventListener('keydown', closeModal);
 	}
 });
