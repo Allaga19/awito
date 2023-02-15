d572353d4030889a685163c5ff43bbae7ef9e9dd
Алла Гаврюшина <Allaga59yandex.ru>
2023-02-15 16:52:38 +0300

    Меняем название у кнопки. Добавляем картинку в модальном окне.

--- js/script.js
+++ js/script.js
@@ -9,15 +9,23 @@ const modalSubmit = document.querySelector('.modal__submit');
 const catalog = document.querySelector('.catalog');
 const modalItem = document.querySelector('.modal__item');
 const modalBtnWarning = document.querySelector('.modal__btn-warning');
+const modalFileInput = document.querySelector('.modal__file-input');
+const modalFileBtn = document.querySelector('.modal__file-btn');
+const modalImageAdd = document.querySelector('.modal__image-add');
+
+//временные константы для картинки и подписи
+const textFileBtn = modalFileBtn.textContent;
+const srcModalImage = modalImageAdd.src;
 
 // форма в модальном окне
 // получаем все элементы из формы
 const elementsModalSubmit = [...modalSubmit.elements]
 	.filter(elem => elem.tagName !== 'BUTTON' && elem.type !== 'submit');   // 58:40 // исключение кнопки
 
-	// база  в localStoreage
+const infoPhoto = {};
+
+// база  в localStoreage
 const saveDB = () => localStorage.setItem('awito', JSON.stringify(dataBase));
-console.log(dataBase);
 
 const checkForm = () => {
 	// прверка на заполнение всех input формы
@@ -38,9 +46,36 @@ const closeModal = (event) => {
 		modalItem.classList.add('hide');
 		document.removeEventListener('keydown', closeModal);
 		modalSubmit.reset();
+		modalImageAdd.src = srcModalImage;
+		modalFileBtn.textContent = textFileBtn;
 		checkForm();
 	}
 };
+
+// photo
+modalFileInput.addEventListener('change', event => {
+	const target = event.target;
+	const reader = new FileReader();
+	const file = target.files[0];
+
+	infoPhoto.filename = file.name;
+	infoPhoto.size = file.size;
+
+	reader.readAsBinaryString(file);
+
+	reader.addEventListener('load', event => {
+		if (infoPhoto.size < 200000) {
+			// меняем название у кнопки
+			modalFileBtn.textContent = infoPhoto.filename;
+			infoPhoto.base64 = btoa(event.target.result);
+			// отображене картнки
+			modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
+		} else {
+			modalFileBtn.textContent = 'Файл не должен привышать 200кб';
+		}
+	});
+});
+
 // События для формы в модальном окне
 modalSubmit.addEventListener('input', checkForm);
 // избавляемся от перезагрузки страницы
@@ -49,10 +84,11 @@ modalSubmit.addEventListener('submit', event => {
 	const itemObj = {};
 	for (const elem of elementsModalSubmit) {
 		itemObj[elem.name] = elem.value;
+		itemObj.image = infoPhoto.base64;
+		dataBase.push(itemObj);
+		closeModal({ target: modalAdd });
+		saveDB();
 	}
-	dataBase.push(itemObj);
-	closeModal({ target: modalAdd });
-	saveDB();
 });
 
 // Отлавливаем событие click и показывает модальное окно
@@ -75,3 +111,4 @@ catalog.addEventListener('click', (event) => {
 // Закрытие модальных окон
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
+// 01:13
