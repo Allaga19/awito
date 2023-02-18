ba967cc89deca052ad3a7cb981c22741f14b1dba
Алла Гаврюшина <Allaga59yandex.ru>
2023-02-18 22:41:37 +0300

    The end

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 // массив для объявления
 const dataBase = JSON.parse(localStorage.getItem('awito')) || [];
-
+let counter = dataBase.length;
 const modalAdd = document.querySelector('.modal__add');
 const addAd = document.querySelector('.add__ad');
 const modalBtnSubmit = document.querySelector('.modal__btn-submit');
@@ -12,6 +12,15 @@ const modalBtnWarning = document.querySelector('.modal__btn-warning');
 const modalFileInput = document.querySelector('.modal__file-input');
 const modalFileBtn = document.querySelector('.modal__file-btn');
 const modalImageAdd = document.querySelector('.modal__image-add');
+
+const modalImageItem = document.querySelector('.modal__image-item');
+const modalHeaderItem = document.querySelector('.modal__header-item');
+const modalStatusItem = document.querySelector('.modal__status-item');
+const modalDescriptionItem = document.querySelector('.modal__description-item');
+const modalCostItem = document.querySelector('.modal__cost-item');
+
+const searchInput = document.querySelector('.search__input');
+const menuContainer = document.querySelector('.menu__container');
 
 //временные константы для картинки и подписи
 const textFileBtn = modalFileBtn.textContent;
@@ -38,40 +47,71 @@ const checkForm = () => {
 // закрытие модального окна
 const closeModal = (event) => {
 	const target = event.target;
+
 	if (target.closest('.modal__close') ||
 		target.classList.contains('modal') ||
 		event.code === 'Escape') {
 		// console.log(1);
-		modalAdd.classList.add('hide');
-		modalItem.classList.add('hide');
-		document.removeEventListener('keydown', closeModal);
-		modalSubmit.reset();
-		modalImageAdd.src = srcModalImage;
-		modalFileBtn.textContent = textFileBtn;
-		checkForm();
+			modalAdd.classList.add('hide');
+			modalItem.classList.add('hide');
+			document.removeEventListener('keydown', closeModal);
+			modalSubmit.reset();
+			modalImageAdd.src = srcModalImage;
+			modalFileBtn.textContent = textFileBtn;
+			checkForm();
 	}
 };
 
+// вывод товара
+const renderCard = (DB = dataBase) => {
+	catalog.textContent = '';
+
+	DB.forEach(item => {
+		catalog.insertAdjacentHTML('beforeend',`
+			<li class="card" data-id="${item.id}">
+				<img class="card__image" src="data:image/jpeg;base64,${item.image}" alt="test">
+				<div class="card__description">
+					<h3 class="card__header">${item.nameItem}</h3>
+					<div class="card__price">${item.costItem} &#8381;</div>
+				</div>
+			</li>
+		`);
+	});
+};
+
+searchInput.addEventListener('input', () => {
+	const valueSearch = searchInput.value.trim().toLowerCase();
+
+	if (valueSearch.length > 2) {
+		// console.log(valueSearch);
+		const result = dataBase.filter(item => item.nameItem.toLowerCase().includes(valueSearch) || item.descriptionItem.toLowerCase().includes(valueSearch));
+		renderCard(result);
+	}
+});
+
 // photo
+// обрабатываем событие смены фотографии при подаче объявления
 modalFileInput.addEventListener('change', event => {
 	const target = event.target;
 	const reader = new FileReader();
-	const file = target.files[0];
+	const file = target.files[0];  //получаем первый элемент - файл с фотографией
 
-	infoPhoto.filename = file.name;
-	infoPhoto.size = file.size;
+	infoPhoto.filename = file.name;  // имя файла
+	infoPhoto.size = file.size;  // размер в байтах
 
 	reader.readAsBinaryString(file);
 
 	reader.addEventListener('load', event => {
-		if (infoPhoto.size < 200000) {
+		if (infoPhoto.size < 300000) {
 			// меняем название у кнопки
 			modalFileBtn.textContent = infoPhoto.filename;
 			infoPhoto.base64 = btoa(event.target.result);
-			// отображене картнки
+			// отображение картинки
 			modalImageAdd.src = `data:image/jpeg;base64,${infoPhoto.base64}`;
 		} else {
-			modalFileBtn.textContent = 'Файл не должен привышать 200кб';
+			modalFileBtn.textContent = 'Файл не должен привышать 300кб';
+			modalFileInput.value = '';
+			checkForm();
 		}
 	});
 });
@@ -82,13 +122,16 @@ modalSubmit.addEventListener('input', checkForm);
 modalSubmit.addEventListener('submit', event => {
 	event.preventDefault();
 	const itemObj = {};
+	
 	for (const elem of elementsModalSubmit) {
 		itemObj[elem.name] = elem.value;
+	}
+		itemObj.id = counter++;
 		itemObj.image = infoPhoto.base64;
 		dataBase.push(itemObj);
-		closeModal({ target: modalAdd });
+		closeModal({target: modalAdd});
 		saveDB();
-	}
+		renderCard();
 });
 
 // Отлавливаем событие click и показывает модальное окно
@@ -98,17 +141,41 @@ addAd.addEventListener('click', () => {
 	document.addEventListener('keydown', closeModal);
 });
 
+
 // получаем модальное окно карточки 
-catalog.addEventListener('click', (event) => {
+catalog.addEventListener('click', event => {
+	const target = event.target;
+	const card = target.closest('.card');
+//функция подготовки модального окна с объявлением для конкретной карточки
+	if (card) {  
+		const item = dataBase.find(obj => obj.id === parseInt(card.dataset.id));
+		// const item = dataBase.find(obj => obj.id === +card.dataset.id);
+
+		modalImageItem.src = `data:image/jpeg;base64,${item.image}`;
+		modalHeaderItem.textContent = item.nameItem;
+		modalStatusItem.textContent = item.status === 'new' ? 'Новый' : 'Б/У';
+		modalDescriptionItem.textContent = item.descriptionItem;
+		modalCostItem.textContent = item.costItem;
+
+		modalItem.classList.remove('hide');  //раскрываем модальное окно покупки
+		document.addEventListener('keydown', closeModal);   //при открытии добавляем обработчик события нажатия на Esc
+	
+	}
+});
+
+menuContainer.addEventListener('click', event => {
 	const target = event.target;
 
-	if (target.closest('.card')) {
-		modalItem.classList.remove('hide');
-		document.addEventListener('keydown', closeModal);
+	if (target.tagName === 'A') {
+		const result = dataBase.filter(item => item.category === target.dataset.category);
+
+		renderCard(result);
 	}
 });
 
 // Закрытие модальных окон
 modalAdd.addEventListener('click', closeModal);
 modalItem.addEventListener('click', closeModal);
-// 01:13
+
+renderCard();
+// 01:03
